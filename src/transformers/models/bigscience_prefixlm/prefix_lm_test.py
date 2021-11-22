dbc5d957a835b3158598b8a1fcaf7322a3cde146
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2021-11-22 15:13:10 +0100

    Test some more things

--- src/transformers/models/bigscience_prefixlm/prefix_lm_test.py
+++ src/transformers/models/bigscience_prefixlm/prefix_lm_test.py
@@ -102,7 +102,8 @@ def prefix_lm_1B3_infer():
     # model = PrefixLMLMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/megatron/1B3-unbiased/megatron/iter_0034500/mp_rank_00")
     # model = PrefixLMLMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/megatron/350M-pile/megatron/iter_0021000/mp_rank_00")
     # model = PrefixLMLMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/megatron/1B3-pile/megatron/iter_0018000/mp_rank_00")
-    model = PrefixLMLMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/megatron/1B3-no-loss-on-targets-only/megatron/iter_0060000/mp_rank_00")
+    # model = PrefixLMLMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/megatron/1B3-no-loss-on-targets-only/megatron/iter_0081000/mp_rank_00")
+    model = PrefixLMLMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/megatron/1B3-rescale-loss-per-position/megatron/iter_0021000/mp_rank_00")
 
     model.eval()
     model.cuda()
@@ -115,10 +116,10 @@ def prefix_lm_1B3_infer():
     # random_inputs = [" Donald Trump will"]
     # random_inputs = [" Barack"]
     # random_inputs = [" The tsar was deposed"]
-    # random_inputs = [" Artificial intelligence will be the best"]
+    random_inputs = [" Artificial intelligence will be the best"]
     # random_inputs = [" Today, researchers have become"]
     # random_inputs = [" In 1991, the remains of Russian Tsar Nicholas II and his family (except for Alexei and Maria) are discovered. The voice of Nicholas's young son, Tsarevich Alexei Nikolaevich, narrates the remainder of the story. 1883 Western Siberia, a young Grigori Rasputin is asked by his father and a group of men to perform magic. Rasputin has a vision and denounces one of the men as a horse thief. Although his father initially slaps him for making such an accusation, Rasputin watches as the man is chased outside and beaten. Twenty years later, Rasputin sees a vision of the Virgin Mary, prompting him to become a priest. Rasputin quickly becomes famous, with people, even a bishop, begging for his blessing." + "\nThe"]
-    random_inputs = [" A novel is a long, fictional narrative which describes intimate human experiences. The novel in the modern era usually makes use of a literary prose style. The development of the prose novel"]
+    # random_inputs = [" A novel is a long, fictional narrative which describes intimate human experiences. The novel in the modern era usually makes use of a literary prose style. The development of the prose novel"]
     # random_inputs = [" In 1982, Trump was listed on the initial Forbes list of wealthy individuals as having a share of his family's estimated $200 million net worth. His financial losses in the 1980s caused him to be dropped from the list between 1990 and 1995.[42] After Trump filed mandatory financial disclosure forms with the Federal Election Commission (FEC) in July 2015, he publicly announced a net worth of about $10 billion while the records released by the FEC showed"]
     # random_inputs = [" A FEW MILES south of Soledad, the Salinas River drops in close to the hillside bank and runs deep and green. The water is warm too, for it has slipped twinkling over the yellow sands in the sunlight before reaching the narrow pool. On one side of the river the golden foothill slopes curve up to the strong and rocky Gabilan mountains, but on the valley side the water is lined with trees - willows fresh and green with every spring, carrying in their lower leaf junctures the debris of the winter’s flooding; and sycamores with mottled, white, recumbent limbs and branches that arch over the pool. On the sandy bank under the trees the leaves lie deep and so crisp that a lizard makes a great skittering if he runs among them. Rabbits"]
     # random_inputs = [" An eleven-time World Champion, he won consecutive World Championship 100 m, 200 m and 4 × 100 metres relay gold medals from 2009 to 2015, with the exception of a 100 m false start in 2011. He is the most successful male athlete of the World Championships. Bolt is the first athlete to win four World Championship titles in the 200 m and is one of the most successful in the 100 m with three titles."]
@@ -206,10 +207,13 @@ def run_perplexity_prefix_lm_varying_context_length():
     tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
     tokenizer.model_max_length = 2048
     # model = PrefixLMLMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/megatron/350M-pile/megatron/iter_0021000/mp_rank_00")
+    # model = PrefixLMLMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/megatron/iter_0048000/mp_rank_00")
     # model = PrefixLMLMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/megatron/1B3-no-loss-on-targets-only/megatron/iter_0060000/mp_rank_00")
-    model = PrefixLMLMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/megatron/1B3-pile/megatron/iter_0018000/mp_rank_00")
+    # model = PrefixLMLMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/megatron/1B3-pile/megatron/iter_0018000/mp_rank_00")
+    model = PrefixLMLMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/megatron/1B3-rescale-loss-per-position/megatron/iter_0021000/mp_rank_00")
     is_prefix = True
-    # model = GPT2LMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/tr3h-350M-oscar-checkpoints/global_step17001")
+    # model = GPT2LMHeadModel.from_pretrained("/home/thomaswang/code/bigscience/checkpoints/tr3d-1B3-oscar-checkpoints/global_step30000")
+    # is_prefix = False
     model.eval()
     model.cuda()
 
@@ -268,20 +272,36 @@ def run_perplexity_prefix_lm_varying_context_length():
                 # attention_mask invariant to batch and head
                 # This is not good as we collapsed unk and pad.
 
-                not_pad_values = (input_ids != tokenizer.pad_token_id)[:, None, None, : ]
+                not_pad_values = (input_ids != tokenizer.pad_token_id)
                 if is_prefix:
                     attention_mask = build_attention_mask(input_ids.shape[-1], n_context, input_ids.device)[None, None,:, :]
-                    attention_mask = not_pad_values * attention_mask
+                    attention_mask = not_pad_values[:, None, None, : ] * attention_mask
                 else:
                     attention_mask = not_pad_values
 
-                output_logits = model(input_ids, attention_mask = attention_mask)['logits']
+                # output_logits = model(input_ids, attention_mask = attention_mask)['logits']
+                #
+                # # select only the ones that matter
+                # labels_mask = (labels != -100)[:, n_context - 1:]
+                # out = output_logits[:, n_context - 1:][labels_mask, :]
+                # labels_out = labels[:, n_context - 1:][labels_mask]
+                # loss = F.cross_entropy(out, labels_out, reduction='none')
+                #
+                # agg_loss += loss.sum()
+                # nb_items += labels_mask.sum()
 
-                # select only the ones that matter
-                labels_mask = (labels != -100)[:, n_context - 1:]
-                out = output_logits[:, n_context - 1:][labels_mask, :]
-                labels_out = labels[:, n_context - 1:][labels_mask]
+                # select only the a portion close to the limit between prefix and suffix
+                end_prediction = n_context - 1 + 4
+                if is_prefix:
+                    attention_mask = attention_mask[:, :, :end_prediction, :end_prediction]
+                else:
+                    attention_mask = attention_mask[:, :end_prediction]
+                output_logits = model(input_ids[:, :end_prediction], attention_mask = attention_mask)['logits']
+                labels_mask = (labels != -100)[:, n_context - 1: end_prediction]
+                out = output_logits[:, n_context - 1: end_prediction][labels_mask, :]
+                labels_out = labels[:, n_context - 1: end_prediction][labels_mask]
                 loss = F.cross_entropy(out, labels_out, reduction='none')
+
                 agg_loss += loss.sum()
                 nb_items += labels_mask.sum()
 
@@ -290,14 +310,22 @@ def run_perplexity_prefix_lm_varying_context_length():
                 # label_ids = labels[:, n_context - 1:]
                 # def convert_ids_to_token(ids):
                 #     return [tokenizer.convert_ids_to_tokens(row) for row in ids]
-                # print(convert_ids_to_token(label_ids))
-                # print(convert_ids_to_token(teacher_forced_predictions))
+                # print("hello")
+                # print(convert_ids_to_token([label_ids[0]]))
+                # print(convert_ids_to_token([teacher_forced_predictions[0]]))
+                #
+                # vocab = tokenizer.get_vocab()
+                # print("hello")
+                # print(torch.gather(output_logits[:, n_context-1], dim=-1, index = labels[:, n_context-1][:, None]).squeeze(-1))
+                # print(f"Check `Ġthe` logit {output_logits[:, n_context-1][:, vocab['Ġthe']]}")
+                # print(f"Check `Ċ` logit {output_logits[:, n_context-1][:, vocab['Ċ']]}")
+                # print(torch.amax(output_logits[0, n_context - 1:], dim=-1))
 
         return agg_loss / nb_items
 
     losses = {}
     print(tokenizer.model_max_length)
-    for n_context in [4, 64, 128, 512, 1024, 1536, 1920, 1984, 2044]:
+    for n_context in [1, 4, 64, 128, 512, 1024, 1536, 1920, 1984, 2044]:
         loss = run_perplexity_prefix_lm(n_context)
         print(f"Context_size: {n_context}, loss: {loss}")
         losses[n_context] = loss
@@ -306,9 +334,29 @@ def run_perplexity_prefix_lm_varying_context_length():
 def main():
     # prefix_invariants()
     # prefix_lm_infer()
-    # prefix_lm_1B3_infer()
+    prefix_lm_1B3_infer()
     # gpt2_1B3_infer()
-    run_perplexity_prefix_lm_varying_context_length()
+    # run_perplexity_prefix_lm_varying_context_length()
 
 if __name__ == "__main__":
     main()
+
+# arc_easy,boolq,copa,headqa,hellaswag,lambada,logiqa,mathqa,mc_taco,mrpc,multirc,openbookqa,piqa,prost,pubmedqa,qnli,qqp,race,rte,sciq,sst,triviaqa,webqs,wic,winogrande,wnli,wsc
+
+## Code to plot the results from varying n_context and then computing loss on the first four tokens.
+# import matplotlib.pyplot as plt
+#
+# n_contexts  = [1, 4, 64, 128, 512, 1024, 1536, 1920, 1984, 2044]
+# tr6f_60k = [7.142319679260254, 7.062319755554199, 3.636333703994751, 3.5352745056152344, 3.159292221069336, 3.3594958782196045, 3.3022403717041016, 2.9234163761138916, 3.179145097732544, 3.205340623855591]
+# tr3d_60k = [5.126442909240723,4.280908584594727,3.5780608654022217,3.4606239795684814,3.0849897861480713,3.295539379119873,3.213402509689331,2.786288022994995,3.0520291328430176,3.1395297050476074]
+# tr3d_30k = [5.152425765991211, 4.331642150878906, 3.665862560272217, 3.5769572257995605, 3.200692892074585, 3.400550127029419, 3.318943500518799, 2.900491952896118, 3.1829869747161865, 3.254850387573242]
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(1,1,1)
+# ax.plot(n_contexts, tr6f_60k, 'r', label="tr6f (60k steps)")
+# ax.plot(n_contexts, tr3d_60k, 'b', label="tr3d (60k steps)")
+# ax.plot(n_contexts, tr3d_30k, 'g', label="tr3d (30k steps)")
+# # ax.set_yscale('log')
+# # ax.set_xscale('log')
+# plt.legend(loc="upper right")
+# fig.show()
